a384f08cf29a3f6267f376a13141115498ce59bc
AntoineCharette <antoineocharette@gmail.com>
2021-11-22 18:50:40 -0500

    changes class components to functional components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-import "./app.scss";
+import "./App.scss";
 
 // Let's talk about using index.js and some other name in the component folder
 // There's pros and cons for each way of doing this ...
 import Header from "./components/header";
 import Footer from "./components/footer";
-import Form from "./components/form";
 import Results from "./components/results";
+import Form from "./components/form";
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -43,5 +43,3 @@ class App extends React.Component {
     );
   }
 }
-
-export default App;
